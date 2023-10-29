b8d95530f4be771689497a547cfb74730733c269
Nicholas Brown <countdooku.steak@gmail.com>
2023-10-29 11:52:15 -0700

    cache inventory counts

--- lib/components/AddToCartButton/getAddToCartButtons.tsx
+++ lib/components/AddToCartButton/getAddToCartButtons.tsx
@@ -2,14 +2,27 @@ import { Product } from "@/lib/models";
 import AddToCartButton from "./AddToCartButton";
 import VariationContent from "./VariationContent";
 import { getInventoryCounts } from "@/lib/square";
-import { ReactElement } from "react";
+import { ReactElement, cache } from "react";
+
+interface InventoryResult {
+  variationIds: string[];
+  inventoryCounts: number[];
+}
+const fetchInventoryCounts = cache(
+  async (product: Product): Promise<InventoryResult> => {
+    const variationIds = Object.keys(product.variations);
+    return {
+      variationIds,
+      inventoryCounts: (await getInventoryCounts(variationIds)).data.counts,
+    };
+  }
+);
 
 // Should not be called by a client component
 export async function getAddToCartButtons(
   product: Product
 ): Promise<Record<string, ReactElement>> {
-  const variationIds = Object.keys(product.variations);
-  const inventoryCounts = (await getInventoryCounts(variationIds)).data.counts;
+  const { variationIds, inventoryCounts } = await fetchInventoryCounts(product);
 
   const buttons = variationIds.map((variationId, i) => {
     const count = inventoryCounts[i];
